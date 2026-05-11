dd9dbb4be268faf5b95cd8e34c99cb423912c642
Cid <contato@ivone.net>
2026-05-11 19:11:21 -0300

    Claude client

---
+++ app/services/claude_service.py
@@ -0,0 +1,8 @@
+from langchain_anthropic import ChatAnthropic
+from app.config.settings import ANTHROPIC_API_KEY
+
+llm = ChatAnthropic(
+    model="claude-3-5-sonnet-latest",
+    anthropic_api_key=ANTHROPIC_API_KEY,
+    temperature=0
+)
